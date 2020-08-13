f553ee0c7cae28b4ebff9d3bbeaab33e88e0b202
Fonti Kar <fonti.kar@gmail.com>
2020-08-13 16:07:34 +1000

    Added my first new function get_icc to computer ICC for any random intercept, documentation and regenerated NAMESPACE

--- R/data.R
+++ R/data.R
@@ -1,3 +1,38 @@
+#'@title Gryphon: Morphological Traits of the mythical Gryphon
+#'@description This dataset was simulated for the paper: "An ecologist's guide to the animal model" by Wilson et. al 2010. Phenotypic traits were simulated over an arbitary pedigree structure using the program 'Pedantics' (Morrissey et al. 2007) to illustrate the uses of the 'animal model'. This dataset includes six variables measured from a cohort of gryphons (n = 1084). Variables include: The ID of each gryphon, the ID of the gryphon's mother, year of birth, sex, birth weight and tarsus length
+#'@usage gryphon
+#'@format A data frame
+#' \enumerate{
+#' \item ANIMAL: Unique identification for each gryphon (1084 levels)
+#' \item MOTHER: Unique identification for each mother (429 levels)
+#' \item BYEAR: Year of Birth
+#' \item SEX:	Sex of gryphon
+#' \item BWT:	Birth weight at fledging
+#' \item TARSUS:	Tarsus length at fledging
+#'}
+#'@name gryphon
+#'@references Wilson, A. J., Reale, D., Clements, M. N., Morrissey, M. M., Postma, E., Walling, C. A., Kruuk, L. E. B & Nussey, D. H. (2010). An ecologist???s guide to the animal model. Journal of Animal Ecology, 79(1), 13-26.
+#'@references Morrissey, M. B., Wilson, A. J., Pemberton, J. M., & Ferguson, M. M. (2007). A framework for power and sensitivity analyses for quantitative genetic studies of natural populations, and case studies in Soay sheep (Ovis aries). Journal of Evolutionary Biology, 20(6), 2309-2321.
+#'@seealso gryphonPed
+#'@docType data
+NULL
+
+#'@title GryphonPed: Pedigree of the mythical Gryphon
+#'@description This dataset is a pedigree and was simulated for the paper: "An ecologist's guide to the animal model" by Wilson et. al 2010. This dataset includes three variables from a cohort of gryphons (n = 1309). Variables include: The ID of each gryphon, the ID of the gryphon's father and the  the ID of the gryphon's mother
+#'@usage gryphonPed
+#'@format A data frame
+#' \enumerate{
+#' \item ANIMAL: Unique identification for each gryphon (1309 levels)
+#' \item FATHER: Unique identification for each father (159 levels)
+#' \item MOTHER: Unique identification for each mother (429 levels)
+#'}
+#'@name gryphonPed
+#'@references Wilson, A. J., Reale, D., Clements, M. N., Morrissey, M. M., Postma, E., Walling, C. A., Kruuk, L. E. B & Nussey, D. H. (2010). An ecologist???s guide to the animal model. Journal of Animal Ecology, 79(1), 13-26.
+#'@references Morrissey, M. B., Wilson, A. J., Pemberton, J. M., & Ferguson, M. M. (2007). A framework for power and sensitivity analyses for quantitative genetic studies of natural populations, and case studies in Soay sheep (Ovis aries). Journal of Evolutionary Biology, 20(6), 2309-2321.
+#'@seealso \code{\link{grphyon}}
+#'@docType data
+NULL
+
 #'@title BTdata: Morphological Traits of the Blue Tit (Cyanistes caeruleus)
 #'@description This dataset was collected for the study: "Testing the phenotypic gambit: phenotypic, genetic and environmental correlations of colour" by Hadfield et. al (2007). Data was collected in the spring of 2002 on a population of blue tits at Silwood Park, London, UK. This dataset includes seven variables measured from a cohort of blue tit chicks (n = 828). Blue tit chicks were cross-fostered between nests (i.e. chicks from different families were swapped). This allowed separation of genetic from environmental sources of variation. Variables include: two morphological traits (tarsus and back); sex; hatching date; the ID of the chick; ID of the chick's mother and the ID of the nest that fostered the chick.
 #'@usage BTdata
---
+++ R/get_icc.R
@@ -0,0 +1,42 @@
+#'@title  Get intra-class correlation
+#'@description Computes the intra-class coefficient from a "brmsfit" model object using the entire posterior distribution
+#'@usage get_icc(model, var)
+#'@param model A model object of class "brmsfit". Model must contains at least one random effect in order to computer an ICC value
+#'@param var A character string that matches a random effect parameter in the model for which ICC is computed for.
+#'@return A data frame containing the variable (var) which ICC is computed for, the posterior mean (Estimate) of ICC value, the standard deviation of the posterior distribution (Est.Error) and the two-sided 95% credible intervals (based on quantiles).
+#'@examples
+#' #Fit an animal model to estimate heritability of birth weight
+#' fit1 <- brm(
+#' BWT ~  1 + (1|ANIMAL) + (1|MOTHER), data = gryphon,
+#' family = gaussian(), cov = list(ANIMAL = A),
+#' chains = 1, cores = 2, iter = 100)
+#'
+#' #Computes ICC for "ANIMAL", this represents the proportion of variance explained by additive genetic variation among individuals (i.e. heritability)
+#' get_icc(model = fit1, var = "ANIMAL")
+#'
+#' Computes ICC for "MOTHER", this is the proportion of variance explained by maternal identity (i.e maternal effects)
+#' get_icc(model = fit1, var = "MOTHER")
+#'@author Fonti Kar - fonti.kar@gmail.com
+#'@export
+
+get_icc <- function(model, var){
+  #string to identify grouping variable
+  var_str <- paste0("sd_", var)
+
+  #Convert SD to variances
+  var_comps <- posterior_samples(model, c("^sd_", "sigma"))^2
+
+  #Calculate the proportion of total variation explained by grouping variable
+  icc <- var_comps[grep(var_str, names(var_comps))] / rowSums(var_comps)
+
+  #Compiling data nicely
+  df <- data.frame(var = var,
+                   Estimate = posterior_summary(icc)[1],
+                   Est.Error = posterior_summary(icc)[2],
+                   Lower =  posterior_summary(icc)[3],
+                   Upper =  posterior_summary(icc)[4])
+
+  rownames(df) <- "icc"
+
+  return(df)
+}
