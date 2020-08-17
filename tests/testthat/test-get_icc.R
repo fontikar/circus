b1f84ccad18e6285f43059e9210bc4a88cb577cd
Fonti Kar <fonti.kar@gmail.com>
2020-08-17 14:13:51 +1000

    get_icc tests

--- tests/testthat/test-get_icc.R
+++ tests/testthat/test-get_icc.R
@@ -14,3 +14,21 @@ test_that("Checking model has random intercepts only", {
   model <- testthis::read_testdata("model.rds")
   expect_true(all(model$ranef$coef %in% "Intercept"))
 })
+
+
+test_that("Checking H2 is about right", {
+  model <- testthis::read_testdata("model.rds")
+  h2 = circus::get_icc(model,var = "ANIMAL")
+  expected_h2 = 0.3183
+  expect_true(abs(h2$Estimate - expected_h2) < 0.01)
+  expect_true(h2$Estimate <= h2$Upper && h2$Estimate >= h2$Lower)
+  expect_true(h2$var == "ANIMAL")
+})
+
+
+test_that("Bad models crash", {
+  for (stupid_model in c("model_bad_2_novar.rds", "model_bad_3_slopes.rds", "model_bad_lm.rds")) {
+      model = testthis::read_testdata("model_bad_2_novar.rds")
+      expect_error(circus::get_icc(model,var = "ANIMAL"))
+  }
+})
